ef28e4607c82ce771da281fd10277e5f27df5255
thivit <thivit@proton.me>
2022-12-03 15:10:05 +0800

    Use data attribute for hovering

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ for (let i = 0; i < 16; i++) {
     grid.append(pixel);
 }
 
+// Change color of pixel when hovered
+grid.addEventListener('mouseover', function(e){
+    e.target.setAttribute('data-hover', 'true');
+})
+grid.addEventListener('mouseout', function(e){
+    e.target.removeAttribute('data-hover');
+})
+
 // Change color of pixel when clicked
 grid.addEventListener('click', function(e){
     e.target.style.backgroundColor = 'blue';
-})
-
-// Change color of pixel when hovered
-grid.addEventListener('mouseover', function(e){
-    e.target.style.backgroundColor = 'lightblue';
-})
-grid.addEventListener('mouseout', function(e){
-    e.target.style.backgroundColor = 'white';
 })
